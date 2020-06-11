0d2b746c5078d9906be36b65be939c10a3779ce9
Filip Kobiec <fipis14@gmail.com>
2020-06-11 13:04:39 +0200

    Add hierarchical navigation

--- AirMonitor/App.xaml.cs
+++ AirMonitor/App.xaml.cs
@@ -10,7 +10,7 @@ namespace AirMonitor
         public App()
         {
             InitializeComponent();
-            MainPage = new MainPage();
+            MainPage = new NavigationPage ( new HomePage());
 
         }
 
---
+++ AirMonitor/Views/HomePage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using AirMonitor.ViewModels;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AirMonitor.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+
+            BindingContext = new HomeViewModel(Navigation);
+        }
+    }
+}
